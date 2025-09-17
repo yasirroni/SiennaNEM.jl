fc4c469df34e39c336c632d54e9ee6cc67289f2c
Muhammad Yasirroni <yasirroni@users.noreply.github.com>
2025-09-17 10:36:15 +1000

    move add_sts! to add_ts

--- src/add_ts.jl
+++ src/add_ts.jl
@@ -4,7 +4,6 @@ function add_ts!(
     interval=nothing,   # auto-detect from data["demand_ts"] if nothing
     scenario_name=1
 )
-
     # TODO: use multiple scenarios as mentioned in
     #   https://nrel-sienna.github.io/PowerSystems.jl/stable/explanation/time_series/#Forecasts
     df_generator = data["generator"]
@@ -56,4 +55,70 @@ function add_ts!(
 
     data["generator_ts"] = df_generator_ts
     data["demand_ts"] = df_demand_ts
+end
+
+# TODO:
+#   1. Support multiple ts
+#   2. Normalize data in p.u. not in here, but in reading data
+function add_sts!(
+    sys::PSY.System,
+    instances::Dict{Int, T},
+    dfs::Dict{Int, S},
+    col::Symbol,
+) where {T <: PSY.PowerLoad, S <: SubDataFrame}
+    units_base_system = get_units_base(sys)
+    set_units_base_system!(sys, "NATURAL_UNITS")
+    for (sym_id, instance) in instances
+        df = copy(dfs[sym_id][:, [col, :date, :value]])
+        df.value .= df.value ./ get_max_active_power(instance)
+        add_st!(sys, instance, df)
+    end
+    set_units_base_system!(sys, units_base_system)
+end
+
+function add_st!(
+    sys::PSY.System,
+    instance::PSY.PowerLoad,
+    df::Union{DataFrame, SubDataFrame},
+)
+    # NOTE: df.value must be already normalized
+    ta = TimeArray(df[:, [:date, :value]], timestamp=:date)
+    sts = SingleTimeSeries(;
+        name="max_active_power",  # 1 means first row
+        data=ta,
+        scaling_factor_multiplier=get_max_active_power,
+    )
+    add_time_series!(sys, instance, sts);
+end
+
+function add_sts!(
+    sys::PSY.System,
+    nested_instances::Dict{Int,Dict{Int,T}},
+    dfs::Dict{Int, S},
+    col::Symbol,
+) where {T <: PSY.RenewableGen, S <: SubDataFrame}
+    units_base_system = get_units_base(sys)
+    set_units_base_system!(sys, "NATURAL_UNITS")
+    for (sym_id, instances) in nested_instances
+        df = copy(dfs[sym_id][:, [col, :date, :value]])
+        max_active_power = get_max_active_power(first(values(instances)))
+        df.value .= df.value ./ max_active_power
+        add_st!(sys, instances, df)
+    end
+    set_units_base_system!(sys, units_base_system)
+end
+
+function add_st!(
+    sys::PSY.System,
+    instances::Dict{Int,T},
+    df::Union{SubDataFrame, DataFrame},
+) where {T <: PSY.RenewableGen}
+    # NOTE: df must be already normalized
+    ta = TimeArray(df[:, [:date, :value]], timestamp=:date)
+    sts = SingleTimeSeries(;
+        name="max_active_power",  # 1 means first row
+        data=ta,
+        scaling_factor_multiplier=get_max_active_power,
+    )
+    add_time_series!(sys, values(instances), sts);
 end
