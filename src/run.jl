8cc5f669c4edbbbaa206b4fba140f4e8644ad26f
Muhammad Yasirroni <yasirroni@users.noreply.github.com>
2025-12-10 16:54:25 +1100

    revert back to manual loop due to bug in SimulationSequence with StorageSystemsSimulations

--- src/run.jl
+++ src/run.jl
@@ -29,20 +29,20 @@ Set up and solve a single unit commitment decision model.
 - A temporary directory is created for problem output and automatically cleaned up.
 """
 function run_decision_model(
-  template::ProblemTemplate,
-  sys::System;
-  kwargs...
-)
-  # Create and solve the decision model
-  problem = DecisionModel(
-    template, sys;
+    template::ProblemTemplate,
+    sys::System;
     kwargs...
-  )
+)
+    # Create and solve the decision model
+    problem = DecisionModel(
+        template, sys;
+        kwargs...
+    )
 
-  build!(problem; output_dir=mktempdir())
-  solve!(problem)
+    build!(problem; output_dir=mktempdir())
+    solve!(problem)
 
-  return OptimizationProblemResults(problem)
+    return OptimizationProblemResults(problem)
 end
 
 """
@@ -100,58 +100,84 @@ to automatically handle:
 - `run_decision_model()` for single-window optimization
 """
 function run_decision_model_loop(
-  template::ProblemTemplate,
-  sys::System;
-  simulation_name::Union{String,Nothing}=nothing,
-  simulation_folder::Union{String,Nothing}=nothing,
-  simulation_steps::Union{Int,Nothing}=nothing,
-  decision_model_kwargs::NamedTuple=(;),
-  simulation_kwargs::NamedTuple=(;),
+    template::ProblemTemplate,
+    sys::System;
+    simulation_name::Union{String,Nothing}=nothing,
+    simulation_folder::Union{String,Nothing}=nothing,
+    simulation_steps::Union{Int,Nothing}=nothing,
+    decision_model_kwargs::NamedTuple=(;),
+    simulation_kwargs::NamedTuple=(;),
 )
 
-  if simulation_name === nothing
-    simulation_name = "DA-UC"
-  end
+    if simulation_name === nothing
+        simulation_name = "DA-UC"
+    end
 
-  if simulation_folder === nothing
-    simulation_folder = mktempdir()
-  else
-    mkpath(simulation_folder)
-  end
+    if simulation_folder === nothing
+        simulation_folder = mktempdir()
+    else
+        mkpath(simulation_folder)
+    end
 
-  if simulation_steps === nothing
-    simulation_steps = IS.get_forecast_window_count(sys.data)
-  end
+    if simulation_steps === nothing
+        simulation_steps = IS.get_forecast_window_count(sys.data)
+    end
 
-  # Create decision model with the provided template
-  decision_model = DecisionModel(
-    template, sys;
-    decision_model_kwargs...
-  )
+    # NOTE:
+    #   Currently, there is a bug in Sienna that make SimulationSequence didn't
+    # work with StorageSystemsSimulations. Thus, we temporarily use manual loop
+    # and return dict of OptimizationProblemResults(problem).
 
-  # Create simulation models container
-  sim_models = SimulationModels(
-    decision_models=[decision_model]
-  )
+    # # Create decision model with the provided template
+    # decision_model = DecisionModel(
+    #   template, sys;
+    #   decision_model_kwargs...
+    # )
 
-  # Define sequence with InterProblemChronology for automatic initial condition propagation
-  sequence = SimulationSequence(
-    models=sim_models,
-    ini_cond_chronology=InterProblemChronology(),
-  )
+    # # Create simulation models container
+    # sim_models = SimulationModels(
+    #   decision_models=[decision_model]
+    # )
 
-  # Create and build simulation
-  sim = Simulation(;
-    models=sim_models,
-    sequence=sequence,
-    name=simulation_name,
-    simulation_folder=simulation_folder,
-    steps=simulation_steps,
-    simulation_kwargs...
-  )
+    # # Define sequence with InterProblemChronology for automatic initial condition propagation
+    # sequence = SimulationSequence(
+    #   models=sim_models,
+    #   ini_cond_chronology=InterProblemChronology(),
+    # )
 
-  build!(sim)
-  execute!(sim)
+    # # Create and build simulation
+    # sim = Simulation(;
+    #   models=sim_models,
+    #   sequence=sequence,
+    #   name=simulation_name,
+    #   simulation_folder=simulation_folder,
+    #   steps=simulation_steps,
+    #   simulation_kwargs...
+    # )
 
-  return SimulationResults(sim)
+    # build!(sim)
+    # execute!(sim)
+
+    # return SimulationResults(sim)
+
+    # Loop through each time slice
+    # TODO
+    # - Use Chronology, that is SimulationSequence.
+    # - Support passing storage last state of charge into initial state of charge on the next loop.
+    # - Support schedule_horizon wider than window_shift to have overlapping time slices.
+    res_dict = Dict{DateTime,OptimizationProblemResults}()
+    for initial_time_slice in InfrastructureSystems.get_forecast_initial_times(sys.data)
+        # Create and solve the decision model with the current time slice
+        problem = DecisionModel(
+            template, sys;
+            horizon=InfrastructureSystems.get_forecast_horizon(sys.data),
+            initial_time=initial_time_slice,
+            decision_model_kwargs...
+        )
+
+        build!(problem; output_dir=mktempdir())
+        solve!(problem)
+        res_dict[initial_time_slice] = OptimizationProblemResults(problem)
+    end
+    return res_dict
 end
