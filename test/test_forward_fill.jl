1a6568cd3c3c2a905147937465bf811314e07c7f
Muhammad Yasirroni <yasirroni@users.noreply.github.com>
2025-12-18 08:39:10 +1100

    refactor test

--- test/test_forward_fill.jl
+++ test/test_forward_fill.jl
@@ -4,7 +4,7 @@ using DataFrames
 using Dates
 
 
-@testset "Time Series Functions Tests" begin
+@testset "Time Series Functions Tests" verbose=true begin
     system_data_dir = "../data/nem12/arrow"
     ts_data_dir = joinpath(system_data_dir, "schedule-1w")
     data = read_system_data(system_data_dir)
